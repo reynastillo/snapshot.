96480fcf69e5859c63ee5e73bd4968873433b586
reynastillo <anheffe@gmail.com>
2019-07-21 15:44:50 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// vanilla JS
+// init with element
+var grid = document.querySelector('.grid');
+var msnry = new Masonry( grid, {
+  // options...
+  itemSelector: '.grid-item',
+  columnWidth: 200
+});
+
+// init with selector
+var msnry = new Masonry( '.grid', {
+  // options...
+});
